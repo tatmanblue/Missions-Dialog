58bbc299403f0a00268dd526302c17a177f148bd
matthew raffel <matt.raffel@gmail.com>
2022-02-16 17:17:10 -0700

    starting dialog spawns
    Signed-off-by: matthew raffel <matt.raffel@gmail.com>

---
+++ MDStudio/Assets/MissionEngine/Code/NPC/DialogNpcSpawnController.cs
@@ -0,0 +1,24 @@
+﻿using TatmanGames.Character;
+using TatmanGames.Character.Interfaces;
+using TatmanGames.Missions.Interfaces;
+using UnityEngine;
+
+namespace TatmanGames.Missions.NPC
+{
+    public class DialogNpcSpawnController : INpcSpawnController
+    {
+        public bool CanSpawnAtStartup(INpcSpawnPoint point)
+        {
+            IDialogNpcSpawnData data = point.Data as IDialogNpcSpawnData;
+            if (null == data)
+                return true;
+
+            return data.AutomaticSpawning;
+        }
+
+        public GameObject[] GetAllNpcSpawnPoints()
+        {
+            return GameObject.FindGameObjectsWithTag(NpcServiceLocator.Instance.Engine?.SpawnTag);
+        }
+    }
+}
